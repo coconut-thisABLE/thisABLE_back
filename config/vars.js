e5157f8e8d858cd35f65f135332b7880f223edae
shunnyjang <suuuu101@naver.com>
2022-03-27 02:14:42 +0900

    :see_no_evil: update: gitignore and add vars

---
+++ config/vars.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+// import .env variables
+require('dotenv-safe').config({
+  path: path.join(__dirname, '../.env'),
+  example: path.join(__dirname, '../.env.example'),
+  allowEmptyValues: true,
+});
+
+module.exports = {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  mongo: {
+    uri: process.env.NODE_ENV === 'test' ?
+         process.env.MONGO_URI_TESTS :
+         process.env.MONGO_URI,
+  },
+  logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
+  base: process.env.BASE_PATH_URL,
+  jwtSecret: process.env.JWT_SECRET,
+  jwtExpirationInterval: process.env.JWT_EXPIRATION_MINUTES,
+  sessionSecret: process.env.SESSION_SECRET,
+  googleProjectID: process.env.GOOGLE_PROJECT_ID,
+  dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
+  dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
+  googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+  googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
+  mongoURI: process.env.MONGO_URI,
+};
