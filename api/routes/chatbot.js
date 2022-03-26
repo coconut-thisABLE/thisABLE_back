18929854abc8160e3d571f22a0bf6ceb5e08b11f
shunnyjang <suuuu101@naver.com>
2022-03-27 02:50:06 +0900

    :sunglasses: #35 docs: update swagger

--- api/routes/chatbot.js
+++ api/routes/chatbot.js
@@ -1,11 +1,55 @@
 const router = require('express').Router();
 const controller = require('../../controllers/chatbot');
 
-// Text Query Route
+/**
+  * @swagger
+  * paths:
+  *   /chatbot/textQuery:
+  *     post:
+  *       tags:
+  *       - ChatBot
+  *       summary: Test Dialogflow Query API
+  *       requestBody:
+  *         required: true
+  *         content:
+  *           application/json:
+  *             schema:
+  *               type: object
+  *               properties:
+  *                 text:
+  *                   type: string
+  *                   example: test
+  *                   required: true
+  *       responses:
+  *         200:
+  *           description: Ok
+  */
 router.route('/textQuery')
     .post(controller.createTextQuery);
 
-// Event Query Route
+/**
+  * @swagger
+  * paths:
+  *   /chatbot/eventQuery:
+  *     post:
+  *       tags:
+  *       - ChatBot
+  *       summary: Test Dialogflow Query API
+  *       requestBody:
+  *         required: true
+  *         content:
+  *           application/json:
+  *             schema:
+  *               type: object
+  *               properties:
+  *                 event:
+  *                   type: string
+  *                   example: welcomeToMyWebsite
+  *                   required: true
+  *       responses:
+  *         200:
+  *           description: Ok
+  */
 router.route('/eventQuery')
     .post(controller.createEventQuery);
 
