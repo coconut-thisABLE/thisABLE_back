3fb7a875e99468a05b492b2ddc8423c197193523
shunnyjang <48276168+shunnyjang@users.noreply.github.com>
2022-03-28 23:50:57 +0900

    :ambulance: hotfix: add error catcher (#41)

--- controllers/chatbot.js
+++ controllers/chatbot.js
@@ -11,51 +11,61 @@ const sessionClient = new dialogflow.SessionsClient();
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
 exports.createTextQuery = async (req, res) => {
-  // We need to send some information that comes from the client to Dialogflow API
+  try {
+    // We need to send some information that comes from the client to Dialogflow API
   // The text query request.
-  const request = {
-    session: sessionPath,
-    queryInput: {
-      text: {
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
         // The query to send to the dialogflow agent
-        text: req.body.text,
-        // The language used by the client (en-US)
-        languageCode: languageCode,
+          text: req.body.text,
+          // The language used by the client (en-US)
+          languageCode: languageCode,
+        },
       },
-    },
-  };
+    };
 
-  // Send request and log result
-  const responses = await sessionClient.detectIntent(request);
-  console.log('Detected intent');
-  const result = responses[0].queryResult;
-  console.log(`  Query: ${result.queryText}`);
-  console.log(`  Response: ${result.fulfillmentText}`);
+    // Send request and log result
+    const responses = await sessionClient.detectIntent(request);
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
 
-  return res.send(result);
+    return res.send(result);
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
 };
 
 exports.createEventQuery = async (req, res) => {
-  // We need to send some information that comes from the client to Dialogflow API
+  try {
+    // We need to send some information that comes from the client to Dialogflow API
   // The text query request.
-  const request = {
-    session: sessionPath,
-    queryInput: {
-      event: {
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        event: {
         // The query to send to the dialogflow agent
-        name: req.body.event,
-        // The language used by the client (en-US)
-        languageCode: languageCode,
+          name: req.body.event,
+          // The language used by the client (en-US)
+          languageCode: languageCode,
+        },
       },
-    },
-  };
+    };
 
-  // Send request and log result
-  const responses = await sessionClient.detectIntent(request);
-  console.log('Detected intent');
-  const result = responses[0].queryResult;
-  console.log(`  Query: ${result.queryText}`);
-  console.log(`  Response: ${result.fulfillmentText}`);
+    // Send request and log result
+    const responses = await sessionClient.detectIntent(request);
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
 
-  return res.send(result);
+    return res.send(result);
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
 };
